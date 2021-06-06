345e9977074151d690b209dfb6227deb13e1ec0d
dcemmerson <emmersod@oregonstate.edu>
2021-06-06 12:23:29 -0700

    Build script to preload image lcp.

---
+++ preloader.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const { readdir } = require('fs/promises');
+
+const pathToEntry = './build/index.html';
+const splitBy = '</title>';
+const builtHTMLContent = fs.readFileSync(pathToEntry).toString();
+const parts = builtHTMLContent.split(splitBy);
+
+let fileWithPreload = [parts[0], splitBy];
+
+const pathToMedia = './build/static/media/';
+const lcpRegex = /^inca_trail\.\w+\.webp/g;
+const relPathArr = pathToMedia.split('build');
+const relPath = relPathArr[relPathArr.length - 1];
+
+readdir(pathToMedia)
+  .then(files => {
+    files.forEach(filename => {
+      if (lcpRegex.test(filename)) {
+        fileWithPreload = [
+          ...fileWithPreload,
+          `<link rel="preload" href="${relPath}${filename}" as="image">`,
+        ];
+      }
+    });
+
+    fileWithPreload = [...fileWithPreload, parts[1]];
+    fs.writeFileSync(pathToEntry, fileWithPreload.join(''));
+  })
+  .catch(err => console.error(err));
